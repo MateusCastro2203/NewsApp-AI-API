903d27ec67e845b7ba662cdb81d3fa8be3a2c02a
Mateus Castro <mateustcastro@gmail.com>
2025-05-08 02:06:29 -0300

    fix: corrigindo retorno da api

--- app/routers/user.py
+++ app/routers/user.py
@@ -2,6 +2,8 @@ import uuid
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from app.database.mongo import db
+import logging
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
@@ -16,18 +18,47 @@ class User(BaseModel):
 
 @router.post("/user")
 async def create_user(userCreate: UserCreate):
-    user_id = str(uuid.uuid4())
-    user_data = userCreate.model_dump()
-    user_data["user_id"] = user_id
-    response = str
     try:
-        if await db["users"].find_one({"email": userCreate.email}):
-            response = {"message": "Usuário já existe", "data": user_data}
-            return {"message": "Usuário já existe", "data": user_data}
+        # Gerar um ID único para o usuário
+        user_id = str(uuid.uuid4())
+        
+        # Converter o modelo para dicionário
+        user_data = userCreate.model_dump()
+        user_data["user_id"] = user_id
+        
+        # Verificar se já existe um usuário com o mesmo email
+        existing_user = await db["users"].find_one({"email": userCreate.email})
+        if existing_user:
+            user_data = await db["users"].find_one({"email": userCreate.email})
+            return JSONResponse(
+                status_code=200,
+                content={"message": "Usuário já cadastrado", "data": {"user_id": user_data["user_id"]}}
+            )
+        
+        # Inserir o novo usuário no banco de dados
         await db["users"].insert_one(user_data)
+        
+        # Remover a senha (se existir) antes de retornar
+
+            
         return {"message": "Usuário criado com sucesso", "data": user_data}
+        
+    except ConnectionError:
+        # Erro de conexão com o MongoDB
+        logging.error("Erro de conexão com o MongoDB", exc_info=True)
+        raise HTTPException(
+            status_code=503,
+            detail="Serviço de banco de dados indisponível. Tente novamente mais tarde."
+        )
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(response,e.message))
+        # Log detalhado para depuração
+        logging.error(f"Erro ao criar usuário: {str(e)}", exc_info=True)
+        
+        # Resposta genérica para o cliente
+        raise HTTPException(
+            status_code=500,
+            detail="Ocorreu um erro ao processar sua solicitação. Entre em contato com o suporte."
+        )
     
 @router.get("/user/{email}")
 async def get_user(email: str):
