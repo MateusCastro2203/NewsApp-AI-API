e8c5cf9b8d7ab4aba3463b1401adafdc6eb85b1c
Mateus Castro <mateustcastro@gmail.com>
2025-05-08 13:25:21 -0300

    fix: adicao de padroes TLS na url do mongo

--- app/database/mongo.py
+++ app/database/mongo.py
@@ -4,6 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URL = os.getenv("MONGO_URL")
-client = AsyncIOMotorClient(MONGO_URL)
+mongo_url = os.getenv("MONGO_URL")
+if mongo_url and "mongodb+srv" in mongo_url:
+    # Adicione parâmetros TLS se não existirem já na URL
+    if "?" in mongo_url:
+        if "&tls=true" not in mongo_url and "&tlsInsecure=false" not in mongo_url:
+            mongo_url += "&tls=true&tlsAllowInvalidCertificates=false"
+    else:
+        mongo_url += "?tls=true&tlsAllowInvalidCertificates=false"
+
+client = AsyncIOMotorClient(mongo_url)
 db = client["newsbot"]
