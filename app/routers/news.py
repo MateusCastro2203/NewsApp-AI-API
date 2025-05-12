197c2260ceb033309b519575813b4cb06534bc47
Mateus Castro <mateustcastro@gmail.com>
2025-05-12 17:45:03 -0300

    fix: correcao de end-point

--- app/routers/news.py
+++ app/routers/news.py
@@ -1,15 +1,16 @@
 from fastapi import APIRouter
 from typing import List
+from app.models.news import NewsItem
 from app.services.news.news_services import fetch_news_with_category, fetch_news_with_query
 
 
 router = APIRouter()
 
-@router.post("/news")
+@router.get("/news", response_model=List[NewsItem])
 async def get_news(category: List[str]):
     return await fetch_news_with_category(category)
 
-@router.post("/news/query")
+@router.get("/news/query", response_model=List[NewsItem])
 async def get_news_with_query(query: str):
     return await fetch_news_with_query(query)
 
